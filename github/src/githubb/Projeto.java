667a1f825b660edc513f90c63442c099ce89fa26
Ingridllo <ingridlouise.unifor@gmail.com>
2023-01-12 20:44:52 -0300

    testando

---
+++ github/src/githubb/Projeto.java
@@ -0,0 +1,16 @@
+package githubb;
+
+public class Projeto {
+
+ private String nome;
+
+ public String getNome() {
+	 return nome; 
+	 
+ }
+ 
+ public void setNome(String nome) {
+      this.nome = nome;
+	
+ } }
+
